8198b358e53c31e706736b762391b53c5075119d
saitoyuu1314 <147906474+saitoyuu1314@users.noreply.github.com>
2024-11-28 10:31:38 +0900

    Update ppy.py

--- ppy.py
+++ ppy.py
@@ -1,1 +1,26 @@
-striafafdsafdsa
+import streamlit as st
+import time
+
+st.title('Streamlit 基本操作')
+
+'スタート'
+
+latest_iteration = st.empty()
+bar = st.progress(30)
+
+'スタート'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Interation {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+expander1 = st.expander('問い合わせ')
+expander1.write('問い合わせ内容を書く')
+expander2 = st.expander('問い合わせ')
+expander2.write('問い合わせ内容を書く')
+expander3 = st.expander('問い合わせ')
+expander3.write('問い合わせ内容を書く')
